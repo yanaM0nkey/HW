7e218820b21d6ded8e9cbb6047de82867c780ac2
yanaM0nkey <ioanna19971408@gmail.com>
2017-05-27 23:57:27 +0300

    dz2

--- dz/dz2/src/dz2/Dz2.java
+++ dz/dz2/src/dz2/Dz2.java
@@ -1,21 +1,111 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dz2;
 
-/**
- *
- * @author UX501
- */
+import java.util.Arrays;
+import java.util.Random;
+
 public class Dz2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        program1();
+        System.out.println("-------------------------------------------------");
+        program2();
+        System.out.println("-------------------------------------------------");
+        program3();
+        System.out.println("-------------------------------------------------");
+        program4();
     }
     
+    public static void program1(){
+        int [] array = printIn(10,100);
+        printOut(array);
+        int min = array[0];
+        int max = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("min value = " + min);
+        System.out.println("max value = " + max);
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == min)
+                array[i] = 0;
+            if(array[i] == max)
+                array[i] = 99;
+        }
+        printOut(array);
+    }
+    
+    public static void program2(){
+        float [] array = printIn(10);
+        printOut(array);
+        float a = 0;
+        for(int i = 0; i < array.length; i++){
+            float elem = array[i];
+            int num = 0;
+            for (int j = 0; j < array.length; j++){
+                if(elem == array[j])
+                    num++;    
+            }
+            if(num != 1 && a != elem){
+                System.out.println("[" + elem + "] - повторений " + num);
+                a = elem;
+            }
+        }
+    }
+    
+    public static void program3(){
+        int [] array = printIn(10,10);
+        printOut(array);
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i] + "   "); 
+        }
+        System.out.println();
+    }
+    
+    public static void program4(){
+        int [] array = printIn(10,10);
+        printOut(array);
+        Arrays.sort(array);
+        printOut(array);
+    }
+    
+    public static void printOut(int [] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "   "); 
+        }
+        System.out.println();
+    }
+    
+    public static void printOut(float [] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "   "); 
+        }
+        System.out.println();
+    }
+    
+    public static int[] printIn(int n, int m){
+        int[] array = new int[n];
+        Random rand = new Random();
+        for(int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(m);
+        }
+        return array;
+    }
+    
+    public static float[] printIn(int n){
+        float[] array = new float[n];
+        Random rand = new Random();
+        for(int i = 0; i < array.length; i++) {
+            array[i] = 1;//rand.nextFloat()*10;
+        }
+        return array;
+    }
+    
+    
+   
 }
+  
